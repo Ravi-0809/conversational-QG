659294809a0a5224344f73978e8c025f865ea076
Ravi-0809 <crbharadwaj@yahoo.com>
2019-07-22 14:10:11 +0530

    changed train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -23,7 +23,9 @@ ${PYT} -u code/train_single.py \
         -qa_sent_enc_layers=1 \
         -copy_attn \
         -reuse_copy_attn \
+        -coref_vocab \
         -lambda_coref_vocab=${LCV} \
+        -coref_attn \
         -lambda_coref_attn=${LCA} \
         -flow \
         -lambda_flow=${LF} \
@@ -31,21 +33,19 @@ ${PYT} -u code/train_single.py \
         -lambda_flow_history=${LFH} \
         -data=${PROJ}/data/processed/coqg.turn${HISTORY_TURN} \
         -save_model=${PROJ}/data/model/${DATE}_turn${HISTORY_TURN}_${MODEL_NAME}_lcv_${LCV}_lca_${LCA}_lf${LF}_lfh_${LFH} \
-        -save_checkpoint_steps=200 \
+        -save_checkpoint_steps=2000 \
         -keep_checkpoint=4 \
         -gpuid=0 \
         -pre_word_vecs_enc=${PROJ}/data/processed/vocab.glove.840B.300d.enc.pt \
         -pre_word_vecs_dec=${PROJ}/data/processed/vocab.glove.840B.300d.enc.pt \
         -batch_size=32 \
-        -valid_steps=200 \
+        -valid_steps=2000 \
         -valid_batch_size=16 \
-        -train_steps=2000 \
+        -train_steps=20000 \
         -optim=adagrad \
         -adagrad_accumulator_init=0.1 \
         -learning_rate=0.1 \
         -learning_rate_decay=0.5 \
         -start_decay_steps=10001 \
         -decay_steps=2000 \
-        -report_every=200 
-	-coref_vocab \
-	-coref_attn
+        -report_every=200
